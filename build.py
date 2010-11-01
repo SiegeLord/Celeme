8b750a81af1cea6c01415442237567ff308355fe
SiegeLord <slabode@aim.com>
2010-11-01 14:20:46 -0400

    Allow passing trailing commands to the program when running it

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 import sys
-import os
+from subprocess import call
+from string import join
 
-os.system('xfbuild +omain +cldc +xldc +xtango *.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -unittest')
+call('xfbuild +omain +cldc +xldc +xtango *.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -unittest', shell=True)
 
 if (len(sys.argv) > 1):
 	if sys.argv[1] == 'run':
-		os.system('./main')
+		call('./main ' + join(sys.argv[2:], ' '), shell=True)

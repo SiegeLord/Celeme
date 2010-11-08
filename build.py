de632cd504a0da5db5727c4fb5451ff3902084fb
SiegeLord <slabode@aim.com>
2010-11-08 14:54:19 -0500

    Fix the build script to not run when there were build errors...

--- build.py
+++ build.py
@@ -4,8 +4,8 @@ import sys
 from subprocess import call
 from string import join
 
-call('xfbuild +omain +cldc +xldc +xtango *.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -unittest', shell=True)
+ret = call('xfbuild +omain +cldc +xldc +xtango *.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -unittest', shell=True)
 
-if (len(sys.argv) > 1):
+if (ret == 0 and len(sys.argv) > 1):
 	if sys.argv[1] == 'run':
 		call('./main ' + join(sys.argv[2:], ' '), shell=True)

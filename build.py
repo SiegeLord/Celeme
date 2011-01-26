4789ea4b4ac317f4b96d675951e46b03ec2d6acc
SiegeLord <slabode@aim.com>
2011-01-26 11:41:40 -0500

    Make the build.py script nicer

--- build.py
+++ build.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python
 
-import sys
+from sys import argv
 from subprocess import call
 from string import join
-import glob
+from glob import glob
 
-#ret = call('xfbuild +D=".deps_celeme" +O=".objs_celeme" +nolink +cldc +xldc +xtango celeme/ -unittest', shell=True)
-#if ret == 0:
-#	ret = call('ar -r libceleme.a .objs_celeme/*.o', shell=True)
-#if ret == 0:
-#	ret = call('xfbuild +omain +cldc +xldc +xtango +xceleme +xopencl main.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -L -L. -L -lceleme -unittest', shell=True)
+opencl_path = '/usr/local/atistream/lib/x86_64'
 
-if len(sys.argv) > 1:
-	if sys.argv[1] == 'lib':
+def shell(cmd):
+	return call(cmd, shell=True)
+
+def dbuild():
+	return shell('xfbuild +omain +cldc +xldc +xtango main.d -L -L' + opencl_path + ' -L -lOpenCL -L -lpthread -L -ldl -unittest')
+
+if len(argv) > 1:
+	if argv[1] == 'lib':
+		# Stupid DMD bugs
 		files = glob.glob('celeme/*.d')
 		files.sort()
 		files.reverse()
 		file_str = ' '.join(files);
 		
-		ret = call('ldc -c ' + file_str +  ' opencl/*.d gnuplot.d -od=".objs_celeme"', shell=True)
+		ret = shell('ldc -c ' + file_str +  ' opencl/*.d gnuplot.d -od=".objs_celeme"')
 		
 		if ret == 0:
-			ret = call('ar -r libceleme.a .objs_celeme/*.o', shell=True)
-	elif sys.argv[1] == 'py':
-		ret = call('xfbuild +D=".deps_pyceleme" +O=".objs_pyceleme" +opy_celeme +cldc +xldc +xtango +xopencl +xceleme pyceleme/main.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -L-L. -L-lceleme -L-lpython2.6 -I. -unittest', shell=True)
-	elif sys.argv[1] == 'doc':
-		ret = call('dil d doc/ --kandil -hl celeme/*.d -version=Doc', shell=True)
-
-if len(sys.argv) == 1 or (len(sys.argv) > 1 and sys.argv[1] == 'run'):
-	ret = call('xfbuild +omain +cldc +xldc +xtango main.d -L -L/usr/local/atistream/lib/x86_64 -L -lOpenCL -L -lpthread -L -ldl -unittest', shell=True)
-	
-
-if ret == 0 and len(sys.argv) > 1:
-	if sys.argv[1] == 'run':
-		call('./main ' + join(sys.argv[2:], ' '), shell=True)
+			ret = shell('ar -r libceleme.a .objs_celeme/*.o')
+	elif argv[1] == 'py':
+		shell('xfbuild +D=".deps_pyceleme" +O=".objs_pyceleme" +opy_celeme +cldc +xldc +xtango +xopencl +xceleme pyceleme/main.d -L -L' + opencl_path + ' -L -lOpenCL -L -lpthread -L -ldl -L-L. -L-lceleme -L-lpython2.6 -I. -unittest')
+	elif argv[1] == 'doc':
+		shell('dil d doc/ --kandil -hl celeme/*.d -version=Doc')
+	elif argv[1] == 'run':
+		ret = dbuild()
+		if ret == 0:
+			shell('./main ' + join(argv[2:], ' '))
+else:
+	dbuild()

7756778ece9f189ffef185c5fe56cbd893e19cc0
SiegeLord <slabode@aim.com>
2011-02-15 21:04:15 -0500

    Added a webpage w/ documentation

---
+++ doc/js/modules.js
@@ -0,0 +1,38 @@
+var g_moduleList = [
+  "celeme.adaptiveheun", "celeme.alignedarray", "celeme.capi", "celeme.celeme",
+  "celeme.clconnector", "celeme.clcore", "celeme.clmodel",
+  "celeme.clneurongroup", "celeme.clrand", "celeme.config",
+  "celeme.configloader", "celeme.frontend", "celeme.heun", "celeme.imodel",
+  "celeme.ineurongroup", "celeme.integrator", "celeme.recorder",
+  "celeme.sourceconstructor", "celeme.util", "celeme.xmlloader",
+  "celeme.xmlutil",
+];
+
+var g_packageTree = new PackageTree(P('', [
+  P('celeme',[
+    M('celeme.adaptiveheun'),
+    M('celeme.alignedarray'),
+    M('celeme.capi'),
+    M('celeme.celeme'),
+    M('celeme.clconnector'),
+    M('celeme.clcore'),
+    M('celeme.clmodel'),
+    M('celeme.clneurongroup'),
+    M('celeme.clrand'),
+    M('celeme.config'),
+    M('celeme.configloader'),
+    M('celeme.frontend'),
+    M('celeme.heun'),
+    M('celeme.imodel'),
+    M('celeme.ineurongroup'),
+    M('celeme.integrator'),
+    M('celeme.recorder'),
+    M('celeme.sourceconstructor'),
+    M('celeme.util'),
+    M('celeme.xmlloader'),
+    M('celeme.xmlutil'),
+  ]),
+])
+);
+
+var g_creationTime = 1297821503;

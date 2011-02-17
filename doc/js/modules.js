c266ce6f67f97ab202dbf0d46bddd5edd0f91889
SiegeLord <slabode@aim.com>
2011-02-17 17:27:26 -0500

    Update docs.

--- doc/js/modules.js
+++ doc/js/modules.js
@@ -1,8 +1,9 @@
 var g_moduleList = [
-  "celeme.adaptiveheun", "celeme.alignedarray", "celeme.capi", "celeme.celeme",
-  "celeme.clconnector", "celeme.clcore", "celeme.clmodel",
-  "celeme.clneurongroup", "celeme.clrand", "celeme.config",
-  "celeme.configloader", "celeme.frontend", "celeme.heun", "celeme.imodel",
+  "celeme.adaptiveheun", "celeme.alignedarray", "celeme.amdperf", "celeme.capi",
+  "celeme.celeme", "celeme.clconnector", "celeme.clcore",
+  "celeme.clmiscbuffers", "celeme.clmodel", "celeme.clneurongroup",
+  "celeme.clrand", "celeme.config", "celeme.configloader", "celeme.frontend",
+  "celeme.heun", "celeme.iclmodel", "celeme.iclneurongroup", "celeme.imodel",
   "celeme.ineurongroup", "celeme.integrator", "celeme.recorder",
   "celeme.sourceconstructor", "celeme.util", "celeme.xmlloader",
   "celeme.xmlutil",
@@ -12,10 +13,12 @@ var g_packageTree = new PackageTree(P('', [
   P('celeme',[
     M('celeme.adaptiveheun'),
     M('celeme.alignedarray'),
+    M('celeme.amdperf'),
     M('celeme.capi'),
     M('celeme.celeme'),
     M('celeme.clconnector'),
     M('celeme.clcore'),
+    M('celeme.clmiscbuffers'),
     M('celeme.clmodel'),
     M('celeme.clneurongroup'),
     M('celeme.clrand'),
@@ -23,6 +26,8 @@ var g_packageTree = new PackageTree(P('', [
     M('celeme.configloader'),
     M('celeme.frontend'),
     M('celeme.heun'),
+    M('celeme.iclmodel'),
+    M('celeme.iclneurongroup'),
     M('celeme.imodel'),
     M('celeme.ineurongroup'),
     M('celeme.integrator'),
@@ -35,4 +40,4 @@ var g_packageTree = new PackageTree(P('', [
 ])
 );
 
-var g_creationTime = 1297821503;
+var g_creationTime = 1297981553;

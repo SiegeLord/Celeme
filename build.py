ea777c5a6dcf604bb0db9c8a54e28f7ff4f3e4a5
SiegeLord <slabode@aim.com>
2011-08-29 16:31:06 -0400

    Make the build script more cross-platform than it was before

--- build.py
+++ build.py
@@ -4,39 +4,45 @@ from sys import argv
 from subprocess import call
 from string import join
 from glob import glob
+from os import remove
 
-opencl_path = '/usr/local/atistream/lib/x86_64'
-amd_perf_str = '-d-version=AMDPerf -L-lGPUPerfAPICL'
-amd_perf_str = ''
-perf_str = '-d-version=Perf'
-perf_str = ''
-
+def rm(file_list):
+	for s in file_list:
+		remove(s)
+		
 def shell(cmd):
 	return call(cmd, shell=True)
 
+opencl_path = '/usr/local/atistream/lib/x86_64'
+perf_str = '-d-version=Perf'
+perf_str = ''
+
+celeme_objs_nocl = glob("celeme/*.d") + glob("celeme/internal/*.d")
+celeme_objs = celeme_objs_nocl + glob("opencl/*.d")
+
 def dbuild():
-	ret = shell('xfbuild +threads=6 +q +omain +cldc +xldc +xtango main.d -g -L -L' + opencl_path + ' -L -lOpenCL -L -lpthread -L -ldl -unittest ' + perf_str + ' ' + amd_perf_str)
-	shell('rm -f *.rsp')
+	ret = shell('xfbuild +threads=6 +q +omain +cldc +xldc +xtango main.d -g -L -L' + opencl_path + ' -L -lOpenCL -L -lpthread -L -ldl -unittest ' + perf_str)
+	rm(glob("*.rsp"))
 	return ret
 
 if len(argv) > 1:
 	if argv[1] == 'lib':
-		ret = shell('ldc -c celeme/*.d celeme/internal/*.d opencl/*.d -od=".objs_celeme"')
+		ret = shell('ldc -c ' + join(celeme_objs) + ' -od=".objs_celeme"')
 		if ret == 0:
-			ret = shell('ar -r libceleme.a .objs_celeme/*.o')
+			ret = shell('ar -r libceleme.a ' + join(glob(".objs_celeme/*.o")))
 	elif argv[1] == 'py':
 		shell('xfbuild +threads=6 +D=".deps_pyceleme" +O=".objs_pyceleme" +opy_celeme +cldc +xldc +xtango +xopencl +xceleme pyceleme/main.d -L -L' + opencl_path + ' -L -lOpenCL -L -lpthread -L -ldl -L-L. -L-lceleme -L-lpython2.6 -I. -unittest')
-		shell('rm -f *.rsp')
+		rm(glob("*.rsp"))
 	elif argv[1] == 'doc':
-		shell('dil d doc/ --kandil -hl celeme/*.d celeme/internal/*.d -version=Doc')
+		shell('dil d doc/ --kandil -hl ' + join(celeme_objs_nocl) + ' -version=Doc')
 	elif argv[1] == 'run':
 		ret = dbuild()
 		if ret == 0:
-			shell('./main ' + join(argv[2:], ' '))
+			shell('./main ' + join(argv[2:]))
 	elif argv[1] == 'c':
 		shell('gcc test.c -o test -L/usr/local/d/ -L. -lceleme -ltango_nomain -lm -ldl -lpthread -L' + opencl_path + ' -lOpenCL -std=c99')
 	elif argv[1] == 'clean':
-		shell('rm ./.objs/*.o')
+		rm(glob(".objs/*.o"))
 		dbuild()
 else:
 	dbuild()
